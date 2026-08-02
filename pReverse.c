00b033e60de7206df155c66dbb05d4767eaaa14c
Alfi-Shahrin-Tamim <bsse1816@iit.du.ac.bd>
2026-08-02 15:59:15 +0600

    Update pReverse.c

--- pReverse.c
+++ pReverse.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void rev(char *word, int size, int i, int j){
      if( j > i){
-        char temp = word[i];
-        word[i] = word[j];
-        word[j] = temp;
+        char temp = *(word + i);
+        *(word + i) = *(word + j);
+        *(word + j) = temp;
         rev(word, size, i + 1, j - 1);
      }
 }
@@ -12,9 +13,16 @@ int main(){
     int size;
     printf("Size of word: ");
     scanf("%d", &size);
-    char word[size + 1];
+    char *word = malloc((size + 1) * sizeof(char));
+
+    if(word == NULL){
+        printf("Memory allocation failed.\n");
+        return 1;
+    }
+
     printf("Text: ");
     scanf("%s", word);
     rev(word, size, 0, size - 1);
     printf("%s\n", word);
+    free(word);
 }
