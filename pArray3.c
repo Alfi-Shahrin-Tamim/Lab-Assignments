d8a02090e2efbf75034887b15131c171678f088c
Alfi-Shahrin-Tamim <bsse1816@iit.du.ac.bd>
2026-08-02 15:05:26 +0600

    Create pArray3.c

---
+++ pArray3.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int y, i, j ,length;
+
+    printf("Size of array: ");
+    scanf("%d", &length);
+    int *num = malloc(length * sizeof(int));
+
+    if( num == NULL){
+        printf("Memory allocation failed.\n");
+        return 1;
+    }
+
+    for(i = 0; i< length;i++){
+        printf("Element at index %d: ", i);
+        scanf("%d", num + i);
+    }    
+    printf("Elements: ");
+    for(i= 0; i < length ; i++){
+        if (i == length - 1) printf("%d \n", *(num + i));       
+        else printf("%d, ", *(num + i));
+    }
+    for(i = 0; i < length; i++){
+        for(j = i + 1;j < length; j++){
+            if (*(num + i) == *(num + j)){
+                for(y = j; y < length - 1; y++) {
+                    *(num + y) = *(num + y + 1);
+                }
+                length--;
+                j--;
+            }
+        }
+    }
+    printf("Updated array: ");
+    for(i= 0; i < length ; i++){
+        if (i == length - 1) printf("%d ", *(num + i));       
+        else printf("%d, ", *(num + i));
+    }
+    printf("\n");  
+    free(num);  
+
+}
